fb50c0394f3cf59423be0101e6b65546b4bb5ba4
ArgaAAL <argalumunon9@gmail.com>
2024-03-17 21:15:28 +0700

    Menambahkan class DataGeneric

---
+++ tpmodul5_1302223038/Main.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        DataGeneric<string> dataGeneric = new DataGeneric<string>("1302223038");
+        dataGeneric.PrintData();
+    }
+}
